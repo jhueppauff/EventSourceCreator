06cce205ce6dcabd927793f901c5adb1643fa6a4
Julian Hüpppauff <julian@hueppauff.info>
2017-10-23 23:16:07 +0200

    Changed .NET FrameWork to 4.6.2

--- EventSourceCreator/Properties/AssemblyInfo.cs
+++ EventSourceCreator/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EventSourceCreator")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Hüppauff 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
